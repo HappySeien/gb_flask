f0f6ce6b7c614d64ad7f62da55778d5bb4651078
Vladislav Goryakin <Happy.Seien@yandex.ru>
2023-04-22 13:09:42 +0300

    l-3
    added db

--- services/apps/__init__.py
+++ services/apps/__init__.py
@@ -2,10 +2,12 @@ from .main_app.views import index
 from .article_app.views import article
 from .reports_app.views import report
 from .user_app.views import user
+from .auth_app.views import auth
 
 VIEWS = [
     index,
     user,
+    auth,
     article,
     report,
 ]
---
+++ services/settings.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+
+
+class Config:
+    _basedir = os.path.abspath(os.path.dirname(__file__))
+
+    DOTENV_PATH = os.path.join(_basedir, '.env')
+
+    if os.path.exists(DOTENV_PATH):
+        load_dotenv(dotenv_path=DOTENV_PATH)
+
+    DEBUG = os.getenv('DEBUG')
+
+    # ADMINS = frozenset(os.getenv('ADMINS'))
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'db.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(_basedir, 'db_migrations')
+    DATABASE_CONNECT_OPTIONS = {}
+
+    # THREADS_PER_PAGE = 8
+
+    # WTF_CSRF_ENABLED = True
+    # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
+
+    # RECAPTCHA_USE_SSL = False
+    # RECAPTCHA_PUBLIC_KEY = 'B?E(G+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v'
+    # RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
+    # RECAPTCHA_OPTIONS = {'theme': 'white'}
