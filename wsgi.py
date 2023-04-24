51fe98caf21be137e85824dd4a68d3615501d7a0
Vladislav Goryakin <Happy.Seien@yandex.ru>
2023-04-24 17:14:41 +0300

    l-4
    migration and docker

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,9 @@
-from werkzeug.security import generate_password_hash
-
 from services.app_manager import create_app
-from services.extension import db
 
 app = create_app()
 
-
-@app.cli.command('init-db', help='create all db')
-def init_db():
-    db.create_all()
-
-
-@app.cli.command('create-users', help='create users')
-def create_users():
-    from services.models import User
-    db.session.add(
-        User(email='test@email.com', password=generate_password_hash('1234'))
+if __name__ == '__main__':
+    app.run(
+        debug=True
     )
-    db.session.commit()
     
